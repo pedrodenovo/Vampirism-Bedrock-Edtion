15fd22e441e69bccbcd2a87aea39af043c055cf2
pedro_denovo <pedrodenovox@gmail.com>
2024-02-07 13:13:46 -0300

    Update main.js

--- Vampirism-BE [BEH]/scripts/main.js
+++ Vampirism-BE [BEH]/scripts/main.js
@@ -1,1 +1,26 @@
 import * as server from "@minecraft/server";
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.system.runInterval(eventData => {
+    var allPlayers = server.world.getAllPlayers()
+    allPlayers.forEach(player => {
+      if (player.hasTag('vamp')){
+        let successCountEntity = player.getEntitiesFromViewDirection[0].entity.runCommand("testfor @s[family=mob,family=!monster,family=!vamp,family=!lob]").successCount
+        if (successCountEntity==1){
+            player.runCommand("title @s times 0 0 0")
+            player.runCommand("title @s title ´&")
+        }
+    }
+});
+},1)
