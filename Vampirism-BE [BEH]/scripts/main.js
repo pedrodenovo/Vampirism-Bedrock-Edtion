7479b58771cb452b0b0b885ed594154657cffb04
pedro_denovo <pedrodenovox@gmail.com>
2024-02-07 16:51:34 +0000

    ui change

--- Vampirism-BE [BEH]/scripts/main.js
+++ Vampirism-BE [BEH]/scripts/main.js
@@ -15,12 +15,16 @@ import * as server from "@minecraft/server";
 server.system.runInterval(eventData => {
     var allPlayers = server.world.getAllPlayers()
     allPlayers.forEach(player => {
-      if (player.hasTag('vamp')){
-        let successCountEntity = player.getEntitiesFromViewDirection[0].entity.runCommand("testfor @s[family=mob,family=!monster,family=!vamp,family=!lob]").successCount
-        if (successCountEntity==1){
-            player.runCommand("title @s times 0 0 0")
-            player.runCommand("title @s title ´&")
+        if (player.hasTag('vamp') && player.getEntitiesFromViewDirection()[0]) {
+            let successCountEntity = player.getEntitiesFromViewDirection()[0].entity.runCommand("testfor @s[family=mob,family=!monster,family=!vamp,family=!lob]").successCount
+            if (successCountEntity == 1) {
+                player.runCommand("title @s times 0 0 0")
+                player.runCommand("title @s title  bite_display")
+                player.getEntitiesFromViewDirection()[0].entity.runCommand("say I see you")
+            }
+
+        }else{
+            player.runCommand("title @s title bite_display")
         }
-    }
-});
-},1)
+    });
+}, 1)
