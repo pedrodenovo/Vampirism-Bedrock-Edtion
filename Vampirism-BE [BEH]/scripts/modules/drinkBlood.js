49e3978a2883c757985fa4340b7ea9f71cf56334
pedro_denovo <pedrodenovox@gmail.com>
2024-02-09 22:58:18 +0000

    09-02-2024-19:57

--- Vampirism-BE [BEH]/scripts/modules/drinkBlood.js
+++ Vampirism-BE [BEH]/scripts/modules/drinkBlood.js
@@ -1,12 +1,48 @@
 import * as server from "@minecraft/server";
 
+const blood_bottleMap = [
+    "sunrise:blood_bottle_0",
+    "sunrise:blood_bottle_1",
+    "sunrise:blood_bottle_2",
+    "sunrise:blood_bottle_3",
+    "sunrise:blood_bottle_4",
+    "sunrise:blood_bottle_5",
+    "sunrise:blood_bottle_6",
+    "sunrise:blood_bottle_7",
+    "sunrise:blood_bottle_8"
+];
+
 function calcularDistancia(x1, y1, z1, x2, y2, z2) {
     const distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2) + Math.pow((z2 - z1), 2));
     return distancia;
 }
 
-server.world.beforeEvents.itemUse.subscribe(eventData => {
-    if (eventData.source.isSneaking && eventData.source.hasTag('vamp') && eventData.source.getEntitiesFromViewDirection()[0] && calcularDistancia(eventData.source.getEntitiesFromViewDirection()[0].entity.location.x, eventData.source.getEntitiesFromViewDirection()[0].entity.location.y, eventData.source.getEntitiesFromViewDirection()[0].entity.location.z, eventData.source.location.x, eventData.source.location.y, eventData.source.location.z) <= 3.25) {
-        eventData.source.getEntitiesFromViewDirection()[0].entity.runCommandAsync("damage @s 5")
+function drainEntityBlood(entity){
+    if (entity.hasTag("drainedBlood_2")){
+        entity.runCommandAsync("tag @s add drainedBlood_3")
+        entity.runCommandAsync("tag @s remove drainedBlood_2")
+    }else if (entity.hasTag("drainedBlood_3")){
+        entity.runCommandAsync("tag @s add drainedBlood_4")
+        entity.runCommandAsync("tag @s remove drainedBlood_3")
+    }else if (entity.hasTag("drainedBlood_4")){
+        entity.runCommandAsync("tag @s add drainedBlood_5")
+        entity.runCommandAsync("tag @s remove drainedBlood_4")
+    }else if (entity.hasTag("drainedBlood_5")){
+        entity.runCommandAsync("kill @s")
+    }else{
+        entity.runCommandAsync("tag @s add drainedBlood_2")
+    }
+}
+
+server.world.afterEvents.itemUse.subscribe(eventData => {
+    let indexItemBottle = blood_bottleMap.indexOf(eventData.itemStack.typeId)
+    let successCountEntity = eventData.source.getEntitiesFromViewDirection()[0].entity.runCommand("testfor @s[family=mob,family=!monster,family=!vamp,family=!lob]").successCount
+    let distanceSquare = eventData.source.getEntitiesFromViewDirection()[0] && calcularDistancia(eventData.source.getEntitiesFromViewDirection()[0].entity.location.x, eventData.source.getEntitiesFromViewDirection()[0].entity.location.y, eventData.source.getEntitiesFromViewDirection()[0].entity.location.z, eventData.source.location.x, eventData.source.location.y, eventData.source.location.z)
+    if (successCountEntity == 1 && indexItemBottle >= 0 && eventData.source.isSneaking && eventData.source.hasTag('vamp') && distanceSquare <= 3.25) {
+        eventData.source.runCommandAsync(`clear @s ${blood_bottleMap[indexItemBottle]} 0 1`)
+        eventData.source.runCommandAsync(`give @s sunrise:blood_bottle_${indexItemBottle + 1} 1`)
+        drainEntityBlood(eventData.source.getEntitiesFromViewDirection()[0].entity)
+    }else if (successCountEntity == 1 &&  eventData.source.isSneaking && eventData.source.hasTag('vamp') && distanceSquare <= 3.25) {
+        drainEntityBlood(eventData.source.getEntitiesFromViewDirection()[0].entity)
     }
 })
--- Vampirism-BE [BEH]/scripts/modules/title_ui_elements.js
+++ Vampirism-BE [BEH]/scripts/modules/title_ui_elements.js
@@ -3,7 +3,38 @@ import * as server from "@minecraft/server";
 function calcularDistancia(x1, y1, z1, x2, y2, z2) {
     const distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2) + Math.pow((z2 - z1), 2));
     return distancia;
-  }
+}
+
+const tagToLevelMap = {
+    "lv_vamp_1": "lv_vamp_1",
+    "lv_vamp_2": "lv_vamp_2",
+    "lv_vamp_3": "lv_vamp_3",
+    "lv_vamp_4": "lv_vamp_4",
+    "lv_vamp_5": "lv_vamp_5",
+    "lv_vamp_6": "lv_vamp_6",
+    "lv_vamp_7": "lv_vamp_7",
+    "lv_vamp_8": "lv_vamp_8",
+    "lv_vamp_9": "lv_vamp_9",
+    "lv_vamp_10": "lv_vamp_10",
+    "lv_vamp_11": "lv_vamp_11",
+    "lv_vamp_12": "lv_vamp_12",
+    "lv_vamp_13": "lv_vamp_13",
+    "lv_vamp_14": "lv_vamp_14"
+};
+
+function drainEntityBlood(entity){
+    if (entity.hasTag("drainedBlood_2")){
+        return "bite_2_display"
+    }else if (entity.hasTag("drainedBlood_3")){
+        return "bite_3_display"
+    }else if (entity.hasTag("drainedBlood_4")){
+        return "bite_4_display"
+    }else if (entity.hasTag("drainedBlood_5")){
+        return "bite_5_display"
+    }else{
+        return "bite_1_display"
+    }
+}
 
 server.system.runInterval(eventData => {
     var allPlayers = server.world.getAllPlayers()
@@ -11,18 +42,21 @@ server.system.runInterval(eventData => {
         let bite_display = ""
         let lv_vamp = ""
         if (player.hasTag('vamp')) {
-            if(player.hasTag("lv_vamp_1")){lv_vamp="lv_vamp_1"}else if(player.hasTag("lv_vamp_2")){lv_vamp="lv_vamp_2"}else if(player.hasTag("lv_vamp_3")){lv_vamp="lv_vamp_3"}else if(player.hasTag("lv_vamp_4")){lv_vamp="lv_vamp_4"}else if(player.hasTag("lv_vamp_5")){lv_vamp="lv_vamp_5"}else if(player.hasTag("lv_vamp_6")){lv_vamp="lv_vamp_6"}else if(player.hasTag("lv_vamp_7")){lv_vamp="lv_vamp_7"}else if(player.hasTag("lv_vamp_8")){lv_vamp="lv_vamp_8"}else if(player.hasTag("lv_vamp_9")){lv_vamp="lv_vamp_9"}else if(player.hasTag("lv_vamp_10")){lv_vamp="lv_vamp_10"}else if(player.hasTag("lv_vamp_11")){lv_vamp="lv_vamp_11"}else if(player.hasTag("lv_vamp_12")){lv_vamp="lv_vamp_12"}else if(player.hasTag("lv_vamp_13")){lv_vamp="lv_vamp_13"}else if(player.hasTag("lv_vamp_14")){lv_vamp="lv_vamp_14"}
-            if (player.getEntitiesFromViewDirection()[0]){
+            if (player.getEntitiesFromViewDirection()[0]) {
                 let successCountEntity = player.getEntitiesFromViewDirection()[0].entity.runCommand("testfor @s[family=mob,family=!monster,family=!vamp,family=!lob]").successCount
                 let distance = calcularDistancia(player.getEntitiesFromViewDirection()[0].entity.location.x, player.getEntitiesFromViewDirection()[0].entity.location.y, player.getEntitiesFromViewDirection()[0].entity.location.z, player.location.x, player.location.y, player.location.z)
                 if (successCountEntity == 1 && distance <= 3.25) {
-                    bite_display = "bite_display"
+                    bite_display = drainEntityBlood(player.getEntitiesFromViewDirection()[0].entity)
                 }
             }
-
+        }
+        for (const tag in tagToLevelMap) {
+            if (player.hasTag(tag)) {
+                lv_vamp = tagToLevelMap[tag];
+                break;
+            }
         }
         player.runCommand("title @s times 0 0 0")
         player.runCommand(`title @s title ${bite_display} ${lv_vamp} x`)
     });
-
 }, 1)
